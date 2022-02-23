3386fbf5f16ad3efd0f0b6e870749d119f2f2b06
dev-frog <kali.nisga@gmail.com>
2022-02-24 03:28:49 +0600

    print string using loop

--- bootSector.asm
+++ bootSector.asm
@@ -3,18 +3,19 @@
 
     mov ah, 0x0e ; int 10/ ah 0x0e  ; fBoot Secotor
     mov bx, String
-    mov al, [bx] ;  memory address of string
-    int 0x10
-
-    mov al, [bx+1] ;  memory address of string
-    int 0x10
-    add bx,2    ;  increment string pointer
-    mov al, [bx] ;  memory address of string
-    int 0x10
+    jmp print_string
 
 
+print_string:
+    mov al, [bx]
+    cmp al, 0
+    je done          ; jump if al == 0
+    int 0x10
+    add bx, 1
+    jmp print_string
 String: db 'F', 'B', '0', '0', 'T', 0
 
+done:
     jmp $
     times 510-($-$$) db 0 ;pads out 0s until we reach the 510th byte
     dw 0xaa55 ;signature bois magic number
